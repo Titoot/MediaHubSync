a75b032a8b10422b9c2f01dbf14bfef2d7b91804
Titoot <82727556+Titoot@users.noreply.github.com>
2024-01-10 06:36:26 +0200

    hide console using debug flag

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-#![windows_subsystem = "windows"]
+#![cfg_attr(
+    all(
+      target_os = "windows",
+      not(debug_assertions),
+    ),
+    windows_subsystem = "windows"
+  )]
 
 use lazy_static::lazy_static;
 use tokio::runtime::Runtime;
