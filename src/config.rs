0f6fe9f6c7a121202afd38ea576e1e1fde8727f4
Titoot <82727556+Titoot@users.noreply.github.com>
2024-01-15 07:55:39 +0200

    add error handling to config deserialize, create Path struct, impl new and from for path and config

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
-use serde::de::value;
 use serde::{Deserialize, Serialize};
 use config::{Config, File as ConfigFile};
+use widestring::U16CString;
+use win_msgbox::Okay;
 
 // use serde_json::{Result as serdeResult, Value};
 use std::fs::File;
@@ -9,13 +9,51 @@ use std::io::Write;
 
 use crate::CONFIG;
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Path {
+   pub path: String,
+   pub srv_path: String,
+   pub folder_type: String,
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MyConfig {
-    pub jwt: String,
-    pub paths: Vec<HashMap<String, String>>,
+   pub api_url: String,
+   pub jwt: String,
+   pub paths: Vec<Path>,
 }
+
+impl Path {
+    fn new() -> Self {
+        Self {
+            path: String::new(),
+            srv_path: String::new(),
+            folder_type: String::new(),
+        }
+    }
+ }
+
+ impl From<(String, String, String)> for Path {
+    fn from((path, srv_path, folder_type): (String, String, String)) -> Self {
+        Self {
+            path,
+            srv_path,
+            folder_type,
+        }
+    }
+ }
+ 
+ impl MyConfig {
+    fn new() -> Self {
+        Self {
+            api_url: String::new(),
+            jwt: String::new(),
+            paths: Vec::new(),
+        }
+    }
+ }
 const CONFIG_PATH: &str = "config.json";
-pub fn read_config() -> Result<MyConfig, std::convert::Infallible> {
+pub fn read_config() -> Result<MyConfig, Box<dyn std::error::Error>> {
 
 
     // Attempt to read the configuration file
@@ -25,14 +63,18 @@ pub fn read_config() -> Result<MyConfig, std::convert::Infallible> {
 
     match config_result {
         Ok(config) => {
-            let my_config: MyConfig = config.try_deserialize().unwrap();
-            Ok(my_config)
+            match config.try_deserialize() {
+                Ok(my_config) => Ok(my_config),
+                Err(e) => {
+                   let message = U16CString::from_str(format!("Failed to Read Config File: {}", e)).unwrap();
+                   let _ = win_msgbox::error::<Okay>(message.as_ptr()).show().unwrap();
+                   Err(Box::new(e))
+                }
+            }
+            
         }
         Err(_) => {
-            let default_config = MyConfig {
-                jwt: String::from(""),
-                paths: vec![HashMap::new()],
-            };
+            let default_config = MyConfig::new();
 
             let default_config_json = serde_json::to_string(&default_config).unwrap();
 
@@ -44,7 +86,7 @@ pub fn read_config() -> Result<MyConfig, std::convert::Infallible> {
     }
 }
 
-pub fn append_path(new_path: HashMap<String, String>) {
+pub fn append_path(new_path: Path) {
     CONFIG.lock().unwrap().paths.push(new_path);
  
     let updated_config_json = serde_json::to_string(&*CONFIG.lock().unwrap()).unwrap();
@@ -64,20 +106,20 @@ pub fn set_jwt(new_jwt: String) {
 
 pub fn delete_path(config: &mut MyConfig, key: String, value: String) {
     config.paths.retain(|path| {
-        path.get(&key) != Some(&value)
+        !(path.path == key && path.srv_path == value)
     });
- 
+  
     let updated_config_json = serde_json::to_string(&*config).unwrap();
- 
+  
     let mut file = File::create(CONFIG_PATH).unwrap();
     file.write_all(updated_config_json.as_bytes()).unwrap();
-}
-
-pub fn check_path(config: &MyConfig, key: String, value: String) -> bool {
+ }
+ 
+ pub fn check_path(config: &MyConfig, key: String, value: String) -> bool {
     for path in &config.paths {
-        if path.contains_key(&key) && path.get(&key) == Some(&value) {
+        if path.path == key && path.srv_path == value {
             return true;
         }
     }
     false
-}
+ }
