5e13f335739968a5b7ae2358bbfa1b808ea34604
Titoot <82727556+Titoot@users.noreply.github.com>
2024-01-15 07:25:10 +0200

    add fs_extra crate, add error handling to sync, remove mock domain, add file size to json

--- src/requests.rs
+++ src/requests.rs
@@ -1,15 +1,29 @@
 use std::collections::HashMap;
+use fs_extra::dir::get_size;
+
+use widestring::U16CString;
+use win_msgbox::Okay;
 
 use crate::CONFIG;
 
 pub async fn sync(path: &str, srv_path: &str) {
+    let api_url = CONFIG.lock().unwrap().api_url.clone();
+    if api_url.is_empty() {
+        log::error!("api url not found in config file");
+        let message = U16CString::from_str("Please add api url to config file").unwrap();
+        let _ = win_msgbox::error::<Okay>(message.as_ptr()).show().unwrap();
+        return;
+    }
+    let size = get_size(path).unwrap();
+    log::debug!("Fize Size: {}", size);
     let json = &serde_json::json!({
             "path": path,
             "srvPath": srv_path,
+            "size": size,
     });
 
     let client = reqwest::Client::new();
-    let res = client.post("https://enbm3oqrsc8c4.x.pipedream.net")
+    let res = client.post(format!("https://{}/", api_url))
         .header("Authorization", format!("Bearer {}",  CONFIG.lock().unwrap().jwt))
         .json(json)
         .send()
