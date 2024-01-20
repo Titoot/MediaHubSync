90e8fb62d0bc00087a222e3174303719a921e4b4
Titoot <82727556+Titoot@users.noreply.github.com>
2024-01-20 18:41:00 +0200

    add rayon and make path loop parallel, remove duplicates, improve structs, add error struct, fix deserializer

--- src/requests.rs
+++ src/requests.rs
@@ -2,6 +2,7 @@ use std::{path::Path, sync::Mutex};
 use fs_extra::dir::get_size;
 use lazy_static::lazy_static;
 use itertools::Itertools;
+use rayon::prelude::*;
 
 use serde::{Serialize, Deserialize};
 use widestring::U16CString;
@@ -25,17 +26,29 @@ lazy_static! {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Error {
+    success: bool,
+    message: String,
+    file_name: Option<String>,
+    parsedfile_name: Option<String>,
+    status_code: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 struct Response {
    success: bool,
    message: String,
+   errors: Vec<Error>,
 }
 
 #[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct PathInfo {
-   fileName: String,
-   srvPath: String,
+   file_name: String,
+   srv_path: String,
    size: u64,
-   fileType: String,
+   file_type: String,
 }
 
 pub async fn sync(paths: Vec<config::Path>) {
@@ -49,20 +62,23 @@ pub async fn sync(paths: Vec<config::Path>) {
 
     UI_STATE.lock().unwrap().set_loading(true);
 
-    let mut path_infos: Vec<PathInfo> = Vec::new();
-    for p in paths {
-        log::debug!("path: {}", p.path);
-        let size = get_size(p.path.clone()).unwrap();
-        log::debug!("File Size: {}", size);
-        let path_info = PathInfo {
-            fileName: Path::new(&p.path).file_name().unwrap().to_str().unwrap().to_string(),
-            srvPath: p.srv_path,
+    let path_infos: Vec<PathInfo> = paths.into_par_iter().map(|p| {
+        let path_name = Path::new(&p.path).file_name().unwrap().to_str().unwrap().to_string();
+        let size = get_size(&p.path).unwrap();
+        PathInfo {
+            file_name: path_name,
+            srv_path: p.srv_path,
             size: size,
-            fileType: p.folder_type,
-        };
-        path_infos.push(path_info);
-    }
-    let json = serde_json::json!(&path_infos);
+            file_type: p.folder_type,
+        }
+    }).collect();
+
+    let unique_path_infos: Vec<_> = path_infos
+    .into_iter()
+    .unique_by(|p| p.file_name.clone())
+    .collect();
+
+    let json = serde_json::json!(&unique_path_infos);
 
     let client = reqwest::Client::new();
     let res = client.post(format!("https://{}/folderSync", api_url))
@@ -78,7 +94,13 @@ pub async fn sync(paths: Vec<config::Path>) {
             log::debug!("Status Code: {}", res.status());
             let json_result: Result<Response, _> = res.json().await;
             match json_result {
-                Ok(json_value) => log::info!("{:?}", json_value),
+                Ok(response) => {
+                    let json_output = serde_json::to_string_pretty(&response);
+                    match json_output {
+                        Ok(json_string) => println!("{}", json_string),
+                        Err(err) => eprintln!("Error serializing to JSON: {:?}", err),
+                    }
+                }
                 Err(e) => log::error!("Failed to deserialize JSON: {}", e),
             }
         },
